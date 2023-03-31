77e05a36bd695cc281250b2eac724e3ed4666ade
Gabriel Kenji Poli Ozaki <g.kenjiJS@hotmail.com>
2023-03-31 00:29:58 -0300

    :sparkles: feat: Cabeçalho

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,24 @@
-import Head from 'next/head'
 import type { AppProps } from 'next/app'
 
 import { globalStyles } from '@/styles/global'
+import logoImg from '../assets/logo.svg'
+import { Container, Header } from '@/styles/pages/app'
+import Head from 'next/head'
 
 globalStyles()
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <Container>
       <Head>
         <title>Ignite Shop App</title>
       </Head>
+
+      <Header>
+        <img src={logoImg} alt="" />
+      </Header>
+
       <Component {...pageProps} />
-    </>
+    </Container>
   )
 }
